fc57998f7620cd351aa36199df98d3e07f8cc741
Soli Tsaagane <51553394+WIZEMVN@users.noreply.github.com>
2020-05-10 19:53:56 +0200

    scaffold edit

--- src/data.js
+++ src/data.js
@@ -1,3 +1,7 @@
 const data = {
-    name : 'Vader Media'
+    x : {
+        name : 'Welcome to Vader Media',
+        msg : `We're busy building something amazing. Just for you.`
+    },
+    copyright : 'Vader Media PTY LTD © 2020'
 }
